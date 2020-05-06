3b961d02c967c75258a9ba6bc1f2a32760516c75
Suyun514 <Suyun514@qq.com>
2020-05-06 11:53:49 +0800

    Simplify code

--- src/index.js
+++ src/index.js
@@ -70,23 +70,19 @@ class Board extends React.Component {
     }
 }
 
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
+function Game() {
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
             </div>
-        );
-    }
+            <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* TODO */}</ol>
+            </div>
+        </div>
+    );
 }
-
-// ========================================
 
 ReactDOM.render(
     <Game />,
